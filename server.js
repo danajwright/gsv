70eef24925f283991727a172bc6d6cb5423ee538
Dana Wright <danajwright@gmail.com>
2016-01-13 14:59:37 -0800

    added partials with express

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
 // app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+
+// app.use(bodyParser.json());
 
 //check if the server is on the Heroku environment, if not, we're on local - include the credentials file
 // if (process.env.SENDGRID_API_KEY === undefined){
@@ -66,6 +67,41 @@ app.use(bodyParser.json());
 // });
 
 app.use(express.static(__dirname + "/public"));
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+// work page 
+app.get('/work', function(req, res) {
+    res.render('pages/work');
+});
+
+// equip page 
+app.get('/equip', function(req, res) {
+    res.render('pages/equip');
+});
+
+// careers page 
+app.get('/careers', function(req, res) {
+    res.render('pages/careers');
+});
+
+// contact page 
+app.get('/contact', function(req, res) {
+    res.render('pages/contact');
+});
 
 app.listen(port);
 console.log('Server listening on ' + port);
