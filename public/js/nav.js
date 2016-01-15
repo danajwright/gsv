f23a79fec2da9887b63712847041204fa0aecd24
Dana Wright <danajwright@gmail.com>
2016-01-14 19:14:29 -0800

    quote modal front end complete

--- public/js/nav.js
+++ public/js/nav.js
@@ -5,20 +5,5 @@ $(document).ready(function() {
     e.preventDefault();
   });
 
-	if (matchMedia) {
-		var mq = window.matchMedia("(min-width: 768px)");
-		mq.addListener(WidthChange);
-		WidthChange(mq);
-	}
-
-	function WidthChange(mq) {
-
-		if (mq.matches) {
-			$('#nav-dropdown').style.display="none";
-		}
-		else {
-			// window width is less than 768px
-		}
-	}
 
 });
