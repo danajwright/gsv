f6289cb0925b6770eb8966d00097174d5d448557
Dana Wright <danajwright@gmail.com>
2016-01-19 16:21:31 -0800

    cut sendgrid from server.js

--- server.js
+++ server.js
@@ -1,35 +1,23 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 
-var sendgrid = require('sendgrid')(SENDGRID_API_KEY);
+// var sendgrid = require('sendgrid')(SENDGRID_API_KEY);
 
-app.post('/email', function(req, resp) {
-  sendgrid.send({
-    to: 'danajwright@gmail.com',
-    from: 'website-quote',
-    subject: 'Quote request from '+req.body.name,
-    text: "bla"
-
-   //  req.body.name
-   //  		req.body.email
-			// req.body.message
-    
-  });
-resp.send("email sent");
-
-//   function(err, json) {
-//     if (err) { return console.error(err); }
-//     console.log(json);
+// app.post('/email', function(req, resp) {
+//   sendgrid.send({
+//     to: 'danajwright@gmail.com',
+//     from: 'website-quote',
+//     subject: 'Quote request from '+req.body.name,
+//     text: "bla",
 //   });
-//   console.log("in email post");
-//   
+//   resp.send("email sent")
 // });
 
 
@@ -40,32 +28,32 @@ app.set('view engine', 'ejs');
 
 // use res.render to load up an ejs view file
 
-// index page 
+// index page
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-// about page 
+// about page
 app.get('/about', function(req, res) {
     res.render('pages/about');
 });
 
-// work page 
+// work page
 app.get('/work', function(req, res) {
     res.render('pages/work');
 });
 
-// equip page 
+// equip page
 app.get('/equip', function(req, res) {
     res.render('pages/equip');
 });
 
-// careers page 
+// careers page
 app.get('/careers', function(req, res) {
     res.render('pages/careers');
 });
 
-// contact page 
+// contact page
 app.get('/contact', function(req, res) {
     res.render('pages/contact');
 });
