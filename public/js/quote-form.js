381c3f7ed7eb3fe483e4e0ca115e323c11e780a4
Dana Wright <danajwright@gmail.com>
2016-01-21 16:16:58 -0800

    quote modal sampleData sending and modal classes updated

--- public/js/quote-form.js
+++ public/js/quote-form.js
@@ -1,14 +1,29 @@
 $(document).ready(function() {
 
-  $('#submitButton').click( function() {
+  var sampleData = {
+    fullName: 'Full Name',
+    email: 'email@bla.com',
+    phone: '7072225555',
+    pickupZip: '94117',
+    pickupDate: '06/03/16',
+    deliverZip: '95401',
+    deliverDate: '06/09/16',
+    cargoValue: '30,000',
+    cargoWeight: '2000',
+  }
+
+  $('#quote-button').on( "click", function(event) {
+    event.preventDefault();
+    console.log('submit button clicked')
       $.ajax({
-          url: '/email',
-          type: 'post',
-          dataType: 'json',
-          data: $('form#quoteForm').serialize(),
-          success: function(data) {
-          console.log(data)
-       }
+        url: '/email',
+        type: 'post',
+        dataType: 'json',
+        
+        data: sampleData,
+        success: function(data) {
+          console.log(data);
+        }
       });
   });
 
--- server.js
+++ server.js
@@ -1,31 +1,46 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
+var SENDGRID_API_KEY = ""
 
-// var sendgrid = require('sendgrid')(SENDGRID_API_KEY);
+var sendgrid = require('sendgrid')(SENDGRID_API_KEY);
 
-// app.post('/email', function(req, resp) {
-//   sendgrid.send({
-//     to: 'danajwright@gmail.com',
-//     from: 'website-quote',
-//     subject: 'Quote request from '+req.body.name,
-//     text: "bla",
-//   });
-//   resp.send("email sent")
-// });
+app.post('/email', function(req, resp) {
+	sendgrid.send({
+	  to: 'danajwright@gmail.com',
+	  from: 'website-quote',
+	  subject: 'Quote request from '+req.body.name,
+	  html: '<b>full name:</b> ' + req.body.fullName +
+	  		'<br><br><b>phone:</b> ' + req.body.phone +
+	  		'<br><br><b>email:</b> ' + req.body.email +
+	  		'<br><br><b>pickup zip:</b> ' + req.body.pickupZip +
+	  		'<br><br><b>pickup date:</b> ' + req.body.pickupDate +
+	  		'<br><br><b>deliver zip:</b> ' + req.body.deliverZip +
+	  		'<br><br><b>deliver date:</b> ' + req.body.deliverDate +
+	  		'<br><br><b>cargo value:</b> ' + req.body.cargoValue +
+	  		'<br><br><b>cargo weight:</b> ' + req.body.cargoWeight
+	});
+
+  console.log(req.body.test)
+  console.log(req.body.phone)
+
+  resp.write(JSON.stringify({blah:"blah response"}));
+  resp.end();
+});
+
 
 app.use(express.static(__dirname + "/public"));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
+// use res.render to load up an ejs view fie
 
 // index page
 app.get('/', function(req, res) {
